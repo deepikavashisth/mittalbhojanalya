4f2d2a503fe146e0aca88af60b2245e357b729c5
Deepika Mittal <your-email@example.com>
2025-07-14 10:08:27 +0530

    Updated settings.py to use environment variables

--- website/settings.py
+++ website/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l=usf*c287@96jl(xd0r(&p0*^q&fnv+!cy2q2n)j#dws=pf&2'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-unsafe-key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
 
 
 # Application definition
